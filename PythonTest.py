771c01651bb64069c8be433434e03b7bd89534b4
moriarity-tim <moriarity.t@northeastern.edu>
2025-05-29 19:21:16 -0400

    large update

---
+++ PythonTest.py
@@ -0,0 +1,14 @@
+import pytest
+from homegrown import multiply
+
+def test_multiply_positive_numbers():
+    assert multiply(2, 3) == 6
+
+def test_multiply_negative_numbers():
+    assert multiply(-1, -2) == 2
+
+def test_multiply_negative&positive_numbers():
+    assert multiply(-1, 4) == -4
+
+def test_multiply_zero():
+    assert multiply(0, 5) == 0
