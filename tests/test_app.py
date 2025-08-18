5b0fedb91c7dd1aabf194c1d5810d9df493f89bc
Tim Moriarity <moriarity.t@northeastern.edu>
2025-08-18 01:36:01 -0400

    try

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 from unittest.mock import MagicMock
-from src.app.app import app, dynamodb, s3, DYNAMODB_TABLE_NAME, S3_BUCKET_NAME
+from app import app, dynamodb, s3, DYNAMODB_TABLE_NAME, S3_BUCKET_NAME
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
